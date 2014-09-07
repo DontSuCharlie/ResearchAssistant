328f90e787c6cc06bedee65f6dd97afa6a473c90
Brian Yang <brian@brianyang.me>
2014-09-07 04:08:26 -0400

    Remove jQuery dependency in favor of plain JS.

--- highlight.js
+++ highlight.js
@@ -32,7 +32,7 @@ function selectText() {
 	}
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", service_url + '?query='+ selection + '&limit=1&output=(description)&indent=false', true);
+    xhr.open("GET", service_url + '?query='+ selection + '&limit=1&output=(description)&indent=true', true);
     xhr.onreadystatechange = function() {
 	    if (xhr.readyState == 4) {
 	    	responseFunction(JSON.parse(xhr.responseText));
@@ -46,21 +46,17 @@ function responseFunction(response) {
 
 	document.getElementById("result_title").innerHTML = selection;
 
-	// Note to self: Remove jQuery dependency! - Brian
-	$('#result').empty();
-    $.each(response.result, function(i, result) {
-      $('<p>', {text:result['output'].description["/common/topic/description"]}).appendTo('#result');
-    });
+	document.getElementById("result").innerHTML = "<p>" + response.result[0].output.description["/common/topic/description"] + "</p>";
 
     document.body.onclick = function(e) {
-    if(e.currentTarget != document.getElementById('popup')) {
-		historyArray[historyArray.length] = selection;
-        document.getElementById("popup").style.height = "0";
-		document.getElementById("popup").scrollTop="0";
-        console.log("Clicked outside!");  
-    } else {
-    	console.log("Clicked inside!");
-    }
+	    if(e.currentTarget != document.getElementById('popup')) {
+			historyArray[historyArray.length] = selection;
+	        document.getElementById("popup").style.height = "0";
+			document.getElementById("popup").scrollTop="0";
+	        //console.log("Clicked outside!");  
+	    } else {
+	    	//console.log("Clicked inside!");
+	    }
     }
 }
 
