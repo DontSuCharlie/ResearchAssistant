60a691d49c4217a9477a17d8020977dd2daafbcd
Brian Yang <brian@brianyang.me>
2014-09-07 08:57:21 -0400

    Fix header gradient

--- highlight.js
+++ highlight.js
@@ -54,6 +54,21 @@ function selectText() {
     xhr.send();
 }
 
+
+function back() {
+	if(index > 0)
+		index--;
+	responseFunction(historyArray[index]);
+}
+function forward() {
+	if(index < historyArray.length)
+		index++;
+	responseFunction(historyArray[index]);
+}
+
+document.getElementById('backButton').onhover = back;
+document.getElementById('forwardButton').onhover = forward;
+
 function responseFunction(response) {
 	document.getElementById("popup").style.height = "27%";
 	document.getElementById("result_title").innerHTML = selection;
@@ -67,16 +82,16 @@ function responseFunction(response) {
 	attribution.innerHTML = "<p class='freebase-attribution'>The above information is provided by the Freebase and licensed under a Creative Commons Generic License (CC-BY). For more information, visit <a href='http://www.freebase.com/' target='_blank'>Freebase.com</a>.</p>";
 
 	historyArray[historyArray.length] = response;
-	currentIndex++;
+	index++;
 
     document.body.onclick = function(e) {
 	    if(e.currentTarget != document.getElementById('popup')) {
 			
-	        document.getElementById("popup").style.height = "0";
+	        //document.getElementById("popup").style.height = "0";
 			document.getElementById("popup").scrollTop="0";
-	        //console.log("Clicked outside!");  
+	        console.log("Clicked outside!");  
 	    } else {
-	    	//console.log("Clicked inside!");
+	    	console.log("Clicked inside!");
 	    }
     }
 }
@@ -85,16 +100,3 @@ document.captureEvents(Event.MOUSEUP);
 document.onmouseup = selectText;
 
 
-function back() {
-	if(index > 0)
-		index--;
-	responseFunction(historyArray[index]);
-}
-function forward() {
-	if(index < historyArray.length)
-		index++;
-	responseFunction(historyArray[index]);
-}
-
-document.getElementById('backButton').onclick = back;
-document.getElementById('forwardButton').onclick = forward;
